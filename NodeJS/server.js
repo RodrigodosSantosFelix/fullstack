b9cb58f9566c405fbe8bfed55b42c658169a1e68
Rodrigo dos Santos Felix <31378833+RodrigodosSantosFelix@users.noreply.github.com>
2019-06-07 14:59:40 -0300

    Add files via upload

---
+++ NodeJS/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const requireDir = require('require-dir');
+const cors = require('cors');
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+var uri = "mongodb://rodrigo:416380123rf.@mycluster-shard-00-00-zeyun.mongodb.net:27017,mycluster-shard-00-01-zeyun.mongodb.net:27017,mycluster-shard-00-02-zeyun.mongodb.net:27017/test?ssl=true&replicaSet=MyCluster-shard-0&authSource=admin&retryWrites=true&w=majority";
+mongoose.connect(uri, {
+    useNewUrlParser: true
+})
+
+requireDir('./src/models');
+
+app.use("/api", require("./src/routes"));
+
+app.listen(3001);
