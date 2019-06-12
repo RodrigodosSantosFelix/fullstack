c3195c5673e6cc5bec272ce6afde641cea835f08
Rodrigo dos Santos Felix <31378833+RodrigodosSantosFelix@users.noreply.github.com>
2019-06-12 10:20:48 -0300

    Update server.js

--- NodeJS/server.js
+++ NodeJS/server.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-var uri = "mongodb://rodrigo:416380123rf.@mycluster-shard-00-00-zeyun.mongodb.net:27017,mycluster-shard-00-01-zeyun.mongodb.net:27017,mycluster-shard-00-02-zeyun.mongodb.net:27017/test?ssl=true&replicaSet=MyCluster-shard-0&authSource=admin&retryWrites=true&w=majority";
+var uri = "mongodb://rodrigo:<passwd>.@mycluster-shard-00-00-zeyun.mongodb.net:27017,mycluster-shard-00-01-zeyun.mongodb.net:27017,mycluster-shard-00-02-zeyun.mongodb.net:27017/test?ssl=true&replicaSet=MyCluster-shard-0&authSource=admin&retryWrites=true&w=majority";
 mongoose.connect(uri, {
     useNewUrlParser: true
 })
